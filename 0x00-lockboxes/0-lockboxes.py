11a2713dac0f5c724bc860769c0dfe4909241abd
RimJoudi <joudirim93@gmail.com>
2021-12-09 23:02:38 +0100

    fix pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,14 +7,15 @@ def canUnlockAll(boxes):
     method that determinesif all the boxes can be opened.
     '''
     n = len(boxes)
-    
+
     seen = {0}
     keys = [0]
 
     while keys:
         u = keys.pop()
         for key in boxes[u]:
-            if isinstance(key, int) and 0 <= key < len(boxes) and key not in seen:
+            if isinstance(key,
+                          int) and 0 <= key < len(boxes) and key not in seen:
                 seen.add(key)
                 keys.append(key)
 
