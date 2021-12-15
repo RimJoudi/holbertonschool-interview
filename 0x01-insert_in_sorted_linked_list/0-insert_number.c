b4665ee8f557ca8610d27499fd1345fcc0ed87d2
RimJoudi <joudirim93@gmail.com>
2021-12-15 23:02:15 +0100

    fix betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_node - a function in C that inserts a number into a sorted singly linked list.
+ * insert_node - a function in C that inserts a number
+ *	into a sorted singly linked list.
  * @head: pointer to the head of the node.
  * @number: number to insert
  * Return: the address of the new node, or NULL if it failed
@@ -10,28 +11,28 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 
-    listint_t *tmp;
-    listint_t *new;
+	listint_t *tmp;
+	listint_t *new;
 
-    tmp = *head;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        return NULL;
-    }
+	tmp = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
-    if (*head == NULL)
-    {
-        *head = tmp;
-    }
+	if (*head == NULL)
+	{
+		*head = tmp;
+	}
 
-    new->n = number;
+	new->n = number;
 
-    while (tmp->next != NULL && tmp->next->n < number)
-        tmp = tmp->next;
+	while (tmp->next != NULL && tmp->next->n < number)
+		tmp = tmp->next;
 
-    new->next = tmp->next;
-    tmp->next = new;
+	new->next = tmp->next;
+	tmp->next = new;
 
-    return new;
+	return (new);
 }
