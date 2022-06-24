3f1cea686de62845d825fe28254335db2d01a5ce
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2022-06-24 21:08:32 +0100

    Palindrome unsigned integer

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "palindrome.h"
+
 /**
-* is_palindrome - function that checks if an unsigned long int is palindrome
-* @n: the unsigned integer to check
-* Return: 1 if n is palindrome, 0 otherwise
-*/
+ * is_palindrome - check palindrome
+ * @n: unsigned long
+ * Return: integer 1 or 0
+ */
 int is_palindrome(unsigned long n)
 {
-  unsigned long int i;
-  unsigned long int j;
-  if (n == 0)
-    return (1);
-  i = n % 10;
-  j = n;*
-    while (j / 10 != 0)
-    {
-      j = j / 10;
-      i = i * 10;
-    }
-  if (j != n % 10)
-    return (0);
-  n = (n - i) / 10;
-  return (is_palindrome(n));
+	unsigned long num, val = 0;
+
+	num = n;
+
+	while (num != 0)
+	{
+		val = val * 10;
+		val = val + num % 10;
+		num = num / 10;
+	}
+	if (n == val)
+		return (1);
+	else
+		return (0);
 }
