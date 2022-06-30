9f5362582218ab54c555dfc7dd0af0c795f2ac73
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2022-06-30 19:32:27 +0100

    the header file

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef SLIDELINE_H
+#define SLIDELINE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif
